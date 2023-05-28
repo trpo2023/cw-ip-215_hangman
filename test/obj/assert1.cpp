ed627ffc426391ebf8246eda0e64aebcd761650d
Max Ivanov <maks07072004@gmail.com>
2023-05-28 22:34:01 +0700

    Ver 3.2

--- test/obj/assert1.cpp
+++ test/obj/assert1.cpp
@@ -7,8 +7,8 @@
 #else
 
 void __assert1_failure(const char* expr, const char* file, int line, const char* func, int error_code) {
-    fprintf(stderr, "assertion failed: %s, file %s, line %d, function %s\n",
-        expr, file, line, func);
+    fprintf(stderr, "TEST [%d / 6] OK \n assertion failed: %s, file %s, line %d, function %s\n",
+        error_code - 1, expr, file, line, func);
     exit(error_code);
 }
 
--- test/obj/main.cpp
+++ test/obj/main.cpp
@@ -7,5 +7,12 @@ int main() {
     test_check_byk_cord();
     test_draw_cross();
     test_draw_gallows();
+
+    cout << "CHECK_BYK_V_SLOVE() - OK" << endl;
+    cout << "CREATE_TEXTUR() - OK" << endl;
+    cout << "LOAD_TEXTUR() - OK" << endl;
+    cout << "CHECK_BYK_CORD() - OK" << endl;
+    cout << "DRAW_CROSS() - OK" << endl;
+    cout << "DRAW_GALLOWS() - OK" << endl;
     
 }
